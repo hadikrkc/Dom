26a0d52d7bf44534618783d18d5a54cfcd28df9d
Hadi KIRKICI <abdulhadi.kirkici@vestel.com.tr>
2021-11-01 17:02:14 +0300

    delete an item and all items

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function eventListeners() {
     //submit event
     form.addEventListener('submit', addNewItem);
 
+    //delete an item
+    taskList.addEventListener('click', deleteItem);
+
+    //delete all items
+    btnDeleteAll.addEventListener('click', deleteAllItems);
+
 }
 
 //add new item
@@ -19,28 +25,59 @@ function addNewItem(e) {
 
     if (input.value === '') {
         alert('add new item');
+    } else {
+
+        //create li
+        const li = document.createElement('li');
+        li.className = 'list-group-item list-group-item-secondary';
+        li.appendChild(document.createTextNode(input.value));
+
+        //create a
+        const a = document.createElement('a');
+        a.className = 'delete-item float-right';
+        a.setAttribute('href', '#');
+        a.innerHTML = '<i class="fas fa-times"></i>';
+
+        //add a to li
+        li.appendChild(a);
+
+
+        //add li to ul
+        taskList.appendChild(li);
+
+        //clear input
+        input.value = '';
     }
 
-    //create li
-    const li = document.createElement('li');
-    li.className = 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
 
-    //create a
-    const a = document.createElement('a');
-    a.className = 'delete-item float-right';
-    a.setAttribute('href', '#');
-    a.innerHTML = '<i class="fas fa-times"></i>';
+    e.preventDefault();
+}
 
-    //add a to li
-    li.appendChild(a);
+//delete an item
+function deleteItem(e) {
 
+    if (confirm('are you sure ?')) {
+        if (e.target.className === 'fas fa-times') {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
 
-    //add li to ul
-    taskList.appendChild(li);
+    e.preventDefault();
+}
 
-    //clear input
-    input.value = '';
+//delete all items
+function deleteAllItems(e) {
 
+    if (confirm('are you sure ?')) {
+        taskList.innerHTML='';
+
+        /*
+        taskList.childNodes.forEach(function (item) {
+            if (item.nodeType === 1) {
+                item.remove();
+            }
+        });
+        */
+    }
     e.preventDefault();
 }
